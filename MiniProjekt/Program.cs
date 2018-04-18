31d51414986f1ab852fc8a38bf27605593e23094
Nitralius <Ferdinand_brodlos@hotmail.com>
2018-04-18 17:47:38 +0200

    Add files via upload

---
+++ MiniProjekt/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniProjekt
+{
+    class Program
+    {
+        static void Main01(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
