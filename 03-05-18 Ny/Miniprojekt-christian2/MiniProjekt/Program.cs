00b77d45c6970d3d63f3c40dc7e6f478b00abbe7
Nitralius <Ferdinand_brodlos@hotmail.com>
2018-05-03 13:52:56 +0200

    Add files via upload

---
+++ 03-05-18 Ny/Miniprojekt-christian2/MiniProjekt/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MiniProjekt
+{
+    /* Dette projekt er lavet af Christian og Ferdinand
+     * 
+     * Beskrivelse af projektet...
+     * 
+     * Christian Gundersen Holmgaard,   studie nr. 20155309
+     * Ferdinand Brødløs,               studie nr. 20167752
+     */
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            var match = new TennisMatch();
+            Console.WriteLine(match);
+        }
+    }
+}
