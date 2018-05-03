a0a4834c29847cdfdba0c7f9230f13b3cdeb77f0
Christian(Atlik/Gunder) <atlikamv@hotmail.com>
2018-05-04 01:02:36 +0200

    ny version

--- MiniProjekt/FileHandler.cs
+++ MiniProjekt/FileHandler.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.IO;
-using Microsoft.VisualBasic;
 using Microsoft.VisualBasic.FileIO;
 
 namespace MiniProjekt
